496780dc599a2901885be78f65d8f6f174e39cdc
Juan Agustin Apaza <111205513+JuanApazaG@users.noreply.github.com>
2024-06-13 16:14:20 -0400

    mejoramos la tarea 2 le agregamos un modo oscuro pero no funciona bien la parte de la raiz

--- TareasAuxiliar/Tarea02/app.js
+++ TareasAuxiliar/Tarea02/app.js
@@ -1,13 +1,15 @@
-val1 = 0;
-val2 = 0;
-section1 = document.getElementById("section1");
-section2 = document.getElementById("section2");
+let val1 = 0;
+let val2 = 0;
+let op = "";
+const section1 = document.getElementById("section1");
+const section2 = document.getElementById("section2");
+
 function escribir(element) {
-  num = element.innerHTML;
+  const num = element.innerHTML;
   section2.innerHTML += num;
 }
 
-buttons = document.querySelectorAll(".button");
+const buttons = document.querySelectorAll(".button");
 
 buttons.forEach((button) => {
   if (!isNaN(button.innerHTML)) {
@@ -17,130 +19,59 @@ buttons.forEach((button) => {
   }
 });
 
-function plus() {
+function setOperation(operator) {
   val1 = parseFloat(section2.innerHTML);
-  section1.innerHTML = `${val1}+`;
+  op = operator;
+  section1.innerHTML = `${val1} ${operator}`;
   section2.innerHTML = "";
 }
 
-plusButton = document.getElementById("plus");
-plusButton.addEventListener("click", () => {
-  plus();
-});
+document.getElementById("plus").addEventListener("click", () => setOperation("+"));
+document.getElementById("minus").addEventListener("click", () => setOperation("-"));
+document.getElementById("multiplication").addEventListener("click", () => setOperation("*"));
+document.getElementById("division").addEventListener("click", () => setOperation("/"));
+document.getElementById("potencia").addEventListener("click", () => setOperation("^"));
+document.getElementById("raiz").addEventListener("click", () => setOperation("√"));
+document.getElementById("fac").addEventListener("click", () => setOperation("!"));
+document.getElementById("log10").addEventListener("click", () => setOperation("log10"));
 
-function minus() {
-  val1 = parseFloat(section2.innerHTML);
-  section1.innerHTML = `${val1}-`;
-  section2.innerHTML = "";
-}
-
-minusButton = document.getElementById("minus");
-minusButton.addEventListener("click", () => {
-  minus();
-});
-
-function multiplication() {
-  val1 = parseFloat(section2.innerHTML);
-  section1.innerHTML = `${val1}*`;
-  section2.innerHTML = "";
-}
-
-multiplicationButton = document.getElementById("multiplication");
-multiplicationButton.addEventListener("click", () => {
-  multiplication();
-});
-
-function division() {
-  val1 = parseFloat(section2.innerHTML);
-  section1.innerHTML = `${val1}/`;
-  section2.innerHTML = "";
-}
-
-divisionButton = document.getElementById("division");
-divisionButton.addEventListener("click", () => {
-  division();
-});
-
-function potencia() {
-    val1 = parseFloat(section2.innerHTML);
-    section1.innerHTML = `${val1}^`;
-    section2.innerHTML = "";
-  }
-  
-  potenciaButton = document.getElementById("potencia");
-  potenciaButton.addEventListener("click", () => {
-    potencia();
-  });
-
-  function raiz() {
-    val1 = parseFloat(section2.innerHTML);
-    section1.innerHTML = `√${val1}`;
-    section2.innerHTML = "";
-  }
-  
-  raizButton = document.getElementById("raiz");
-  raizButton.addEventListener("click", () => {
-    raiz();
-  });
-
-  function factorial() {
-    val1 = parseFloat(section2.innerHTML);
-    section1.innerHTML = `${val1}!`;
-    section2.innerHTML = "";
-  }  
-  facButton = document.getElementById("fac");
-  facButton.addEventListener("click", () => {
-    factorial();
-  });
-  function log10() {
-    val1 = parseFloat(section2.innerHTML);
-    section1.innerHTML = `${val1}!`;
-    section2.innerHTML = "";
-  }  
-  logd10 = document.getElementById("log10");
-  logd10.addEventListener("click", () => {
-    log10();
-  });
 function equal() {
-  op = section1.innerHTML[section1.innerHTML.length - 1];
-  if (section2.innerHTML.length > 0) val2 = parseInt(section2.innerHTML);
+  if (section2.innerHTML.length > 0) val2 = parseFloat(section2.innerHTML);
   else val2 = 0;
+  let res;
   switch (op) {
     case "+":
       res = val1 + val2;
-      section1.innerHTML = `${val1}+${val2}`;
+      section1.innerHTML = `${val1} + ${val2}`;
       section2.innerHTML = `${res}`;
       break;
     case "-":
       res = val1 - val2;
-      section1.innerHTML = `${val1}-${val2}`;
+      section1.innerHTML = `${val1} - ${val2}`;
       section2.innerHTML = `${res}`;
       break;
     case "*":
       res = val1 * val2;
-      section1.innerHTML = `${val1}*${val2}`;
+      section1.innerHTML = `${val1} * ${val2}`;
       section2.innerHTML = `${res}`;
       break;
     case "/":
       res = val1 / val2;
-      section1.innerHTML = `${val1}/${val2}`;
+      section1.innerHTML = `${val1} / ${val2}`;
       section2.innerHTML = `${res}`;
       break;
-    case "potencia":
-      res = val1 ** val2;
+    case "^":
+      res = Math.pow(val1, val2);
       section1.innerHTML = `${val1} ^ ${val2}`;
       section2.innerHTML = `${res}`;
       break;
-    case "raiz":
-      res =   Math.sqrt(val1);
+    case "√":
+      res = Math.sqrt(val1);
       section1.innerHTML = `√${val1}`;
       section2.innerHTML = `${res}`;
       break;
-    case "fac":
-      res = factorials(val1);
-      function factorials(n) {
-        return n === 0 ? 1 : n * factorials(n - 1);
-      }
+    case "!":
+      res = factorial(val1);
       section1.innerHTML = `${val1}!`;
       section2.innerHTML = `${res}`;
       break;
@@ -154,28 +85,24 @@ function equal() {
   }
 }
 
-equalButton = document.getElementById("equal");
-equalButton.addEventListener("click", () => {
-  equal();
-});
+function factorial(n) {
+  return n === 0 ? 1 : n * factorial(n - 1);
+}
+
+document.getElementById("equal").addEventListener("click", () => equal());
 
 function del() {
   section2.innerHTML = section2.innerHTML.slice(0, -1);
 }
 
-delButton = document.getElementById("delete");
-delButton.addEventListener("click", () => {
-  del();
-});
+document.getElementById("delete").addEventListener("click", () => del());
 
 function clean() {
   section1.innerHTML = "";
   section2.innerHTML = "";
+  val1 = 0;
+  val2 = 0;
+  op = "";
 }
 
-cleanButton = document.getElementById("clean");
-cleanButton.addEventListener("click", () => {
-  clean();
-});
-
-/*Agregar una fila de botones con las funciones de potencia, raiz, factorial, logaritmo base 10*/
+document.getElementById("clean").addEventListener("click", () => clean());
